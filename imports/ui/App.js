7be2552a88a9425ccfc82e28841fcfff13090936
Ayrton <ayrton@kelda.no>
2019-01-24 13:02:11 +0100

    Added Meteor Users

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Game from './Game'
 
+import { withTracker } from 'meteor/react-meteor-data';
+import Links from '../api/links.js'
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+
 class App extends Component {
 
   render() {
+
         return (
-            <Game/>
+            <div>
+                <AccountsUIWrapper />
+                <Game/>
+            </div>
+
         );
       }
 }
+
+App = withTracker(() => {
+    return {
+        links: Links.find({}).fetch(),
+    };
+})(App);
 
 export default App;
